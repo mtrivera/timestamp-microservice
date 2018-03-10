659e4f7683b255d71ec9e75bb03f079130a0f799
Glitch (ahead-celsius) <none>
2018-03-10 06:22:33 +0000

    👧🎏 Pass input to generate output

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // init project
 var strftime = require('strftime');
+var isDate = require('validate.io-strict-date');
 var express = require('express');
 var app = express();
 
@@ -16,16 +17,29 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
+/*
 app.get("/dreams", function (request, response) {
   response.send(dreams);
 });
-
-app.get('/:query', (req, res) => {
-  res.send(req.params);
-  console.log(req.query);
+*/
+app.param('userInput', (req, res, next, userInput) => {
+  // 1. Check if valid date
+  var timestamp = userInput;
+  // 2. If valid date, return object with unix and natural with values
+  // TODO: Check if input is valid
+  if (isDate(new Date(timestamp))) {
+    req.userInput = { "unix": timestamp, "natural": strftime('%B %d, %Y', new Date(Number(timestamp)))};
+  } else {
+  // 3. If invalid date, return obejct with null for both values
+    req.userInput = { "unix": null, "natural": null };
+  }  
+  next();
 });
 
+app.get('/:userInput', (req, res) => {
+  res.json(req.userInput);
+});
+/*
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/dreams", function (request, response) {
   dreams.push(request.query.dream);
@@ -38,7 +52,7 @@ var dreams = [
   "Climb a really tall mountain",
   "Wash the dishes"
 ];
-
+*/
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
